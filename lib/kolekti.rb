e9a4058e4490c1b8349026805d83d68fd23bc682
Daniel Miranda <danielkza2@gmail.com>
2015-12-11 16:43:15 -0200

    Allow access to currently registered collectors and unregistration
    Signed off by: Heitor Reis <marcheing@gmail.com>

--- lib/kolekti.rb
+++ lib/kolekti.rb
@@ -8,5 +8,13 @@ module Kolekti
   def self.register_collector(collector)
     COLLECTORS << collector
   end
+  def self.collectors
+    COLLECTORS
+  end
+
+  def self.unregister_collector(collector)
+    raise ArgumentError.new("Collector #{collector} was not registered!") unless COLLECTORS.include? collector
+    COLLECTORS.delete collector
+  end
 end
 
--- spec/kolekti_spec.rb
+++ spec/kolekti_spec.rb
@@ -1,16 +1,50 @@
 require 'spec_helper'
 
 describe Kolekti do
+  let(:collector) { FactoryGirl.build(:collector) }
+
   it 'has a version number' do
     expect(Kolekti::VERSION).not_to be nil
   end
 
   describe 'methods' do
-    describe 'register_collector' do
-      it 'is expected to add a collector to the list' do
-        Kolekti.register_collector(Object)
+    context 'altering collectors state' do
+      before :each do
+        Kolekti.register_collector(collector)
+      end
 
-        expect(Kolekti::COLLECTORS).to include(Object)
+      after :each do
+        Kolekti.unregister_collector(collector)
+      end
+
+      describe 'register_collector' do
+        it 'is expected to add a collector to the list' do
+          expect(Kolekti::COLLECTORS).to include(collector)
+        end
+      end
+
+      describe 'collectors' do
+        it 'is expected to return the current collectors' do
+          expect(Kolekti.collectors).to include(collector)
+        end
+      end
+    end
+
+    describe 'unregister_collector' do
+      context 'with an unregistered collector' do
+        it 'is expected to raise an ArgumentError' do
+          expect { Kolekti.unregister_collector(collector) }.to raise_error(ArgumentError)
+        end
+      end
+
+      context 'with a registered collector' do
+        before do
+          Kolekti.register_collector(collector)
+        end
+        it 'is expected to remove the collector from the list' do
+          Kolekti.unregister_collector(collector)
+          expect(Kolekti::COLLECTORS).not_to include collector
+        end
       end
     end
   end
