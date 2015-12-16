341eb2dce03297db03e1b81002c3f56455ee166e
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2015-12-16 17:52:50 -0200

    Add initialization methods to Entities::Base
    The expected parsers use lots of calls to create. Having this method
    helps when importing those collectors.

--- lib/kolekti/entities/base.rb
+++ lib/kolekti/entities/base.rb
@@ -1,10 +1,17 @@
 module Kolekti
   module Entities
     class Base
+      def initialize(attributes={})
+        attributes.each { |field, value| send("#{field}=", value) }
+      end
+
       def save
         self.freeze # Ensure that no changes are made after the object has been queued for persistence
         Kolekti::Entities::PERSISTENCE_QUEUE.enq(self)
+      end
 
+      def self.create(attributes={})
+        self.new(attributes).save
       end
     end
   end
--- spec/entities/base_spec.rb
+++ spec/entities/base_spec.rb
@@ -1,7 +1,27 @@
 require 'spec_helper'
 
+class TestBase < Kolekti::Entities::Base
+  attr_accessor :test
+end
+
 describe Kolekti::Entities::Base do
-  describe 'methods' do
+  describe 'initialization' do
+    context 'with invalid attributes' do
+      it 'is expected to raise a NoMethodError' do
+        expect { described_class.new({invalid_attr: 1}) }.to raise_error(NoMethodError)
+      end
+    end
+
+    context 'with a valid attribute' do
+      subject { TestBase.new(test: 'test') }
+
+      it 'is expected to fill in the attribute' do
+        expect(subject.test).to eq('test')
+      end
+    end
+  end
+
+  describe 'method' do
     describe 'save' do
       it 'is expected to put that record on the persistence queue and freeze the object' do
         expect(Kolekti::Entities::PERSISTENCE_QUEUE).to be_empty # Ensure the Queue is empty
@@ -10,6 +30,20 @@ describe Kolekti::Entities::Base do
 
         expect(Kolekti::Entities::PERSISTENCE_QUEUE.pop).to eq(subject)
         expect(subject.frozen?).to be_truthy
+      end
+    end
+  end
+
+  describe 'class method' do
+    describe 'create' do
+      let(:attributes) { {test: 'test'} }
+      let(:instance) { mock('instance') }
+
+      it 'is expected to instantiate a new object and save it' do
+        instance.expects(:save)
+        described_class.expects(:new).with(attributes).returns(instance)
+
+        described_class.create(attributes)
       end
     end
   end
