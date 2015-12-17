810da4bfaacfd372235e267a64acacebe28c3f2b
Daniel Miranda <danielkza2@gmail.com>
2015-12-17 14:51:56 -0200

    Add git clone to get a repository on analizo runner test
    Signed off by: Heitor Reis <marcheing@gmail.com>

--- spec/kolekti/analizo/collector_spec.rb
+++ spec/kolekti/analizo/collector_spec.rb
@@ -1,16 +1,38 @@
+require 'tmpdir'
+require 'fileutils'
+
 require 'spec_helper'
 require 'kalibro_client'
 
 describe Kolekti::Analizo::Collector do
   describe 'run_wanted_metrics' do
-    let(:metric_configurations) { [FactoryGirl.build(:metric_configuration, metric: FactoryGirl.build(:metric, code: 'total_eloc', scope: KalibroClient::Entities::Miscellaneous::Granularity::SOFTWARE))] }
+    let(:metric_configurations) { [FactoryGirl.build(:metric_configuration)] }
     let(:strategy) { Kolekti::TestPersistenceStrategy.new }
     subject { described_class.new }
-    let(:runner) { Kolekti::Runner.new('/home/marcheing/mezuro/sbking', metric_configurations, strategy) }
 
-    it 'is expected to run the wanted metrics' do
-      Kolekti.register_collector(subject)
-      runner.run_wanted_metrics
+    context 'with a C++ repository' do
+      let(:repository_path) { Dir::mktmpdir }
+      let(:runner) { Kolekti::Runner.new(repository_path, metric_configurations, strategy) }
+
+      before :each do
+        Kolekti.register_collector(subject)
+      end
+
+      after :each do
+        Kolekti.unregister_collector(subject)
+      end
+
+      it 'is expected to run the wanted metrics' do
+        begin
+          # FIXME: Find a beter way to clone and clean the repository to be analized
+          unless `git clone -q 'https://github.com/rafamanzo/runge-kutta-vtk.git' '#{repository_path}'`
+            raise StandardError.new('git clone failed')
+          end
+          runner.run_wanted_metrics
+        ensure
+          FileUtils.rm_rf(repository_path)
+        end
+      end
     end
   end
 end
