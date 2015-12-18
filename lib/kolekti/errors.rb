09cd54951ff2d9bf3fe681873cd9b2818d3b415b
Daniel Miranda <danielkza2@gmail.com>
2015-12-18 15:50:57 -0200

    Fix missing module declaratio in errors.rb and create CollectorError
    Signed off by: Heitor Reis <marcheing@gmail.com>

--- lib/kolekti/errors.rb
+++ lib/kolekti/errors.rb
@@ -1,3 +1,6 @@
-class KolektiError < RuntimeError; end
+module Kolekti
+  class KolektiError < RuntimeError; end
 
-class UnavailableMetricError < KolektiError; end
+  class UnavailableMetricError < KolektiError; end
+  class CollectorError < KolektiError; end
+end
