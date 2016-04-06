9048c33844d3618f4a32cf3b86fed644e02a90cb
Diego Araújo <diegoamc@protonmail.ch>
2016-04-06 15:09:16 -0300

    Create a base parser
    This is heavily based on KalibroProcessor's MetricFu and Radon base
    implementations.
    
    Signed off by: Rafael Reggiani Manzo <rr.manzo@protonmail.com>

---
+++ spec/kolekti/parser_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe Kolekti::Parser do
+  describe 'methods' do
+    describe 'parse' do
+      it 'is expected to raise a NotImplementedError' do
+        expect { described_class.parse(nil) }.to raise_error(NotImplementedError)
+      end
+    end
+
+    describe 'default_value' do
+      it 'is expected to raise a NotImplementedError' do
+        expect { described_class.default_value }.to raise_error(NotImplementedError)
+      end
+    end
+
+    describe 'parse_file_name' do
+      cases = {
+       'out' => 'out',
+       'out/' => 'out',
+       'out.txt' => 'out',
+       'out_1.txt' => 'out_1',
+       'in.out.1.txt' => 'in_out_1',
+       '/out/in.1.txt' => 'out.in_1',
+       '/out.2/in_1' => 'out_2.in_1',
+       'out.3/out.2/in_1' => 'out_3.out_2.in_1'
+      }
+
+      cases.each do |input, output|
+        it "is expected to receive '#{input}' and to respond with '#{output}'" do
+          expect(described_class.parse_file_name(input)).to eq(output)
+        end
+      end
+    end
+  end
+end
